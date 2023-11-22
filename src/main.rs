042100d9e014a676c257600543ca00914124817f
JoshBenn <jdbenn@asu.edu>
2023-11-22 12:30:18 -0700

    Implementations partially completed

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,19 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 fn collision_add_list(c: &mut Criterion) {
     let mut group = c.benchmark_group("Collision_Add_List");
-
+    let num_buckets = 10;
+    let num_items = 100;
+    
     group.bench_function("custom_linked_list", |b| {
-        
+        let mut map = CustomListMap::<u32, u32>::new(num_buckets);
+
+        b.iter(|| {
+            for i in 0..num_items {
+                map.insert(i % 5, i);
+            }
+        });
+
+        group.finish();
     });
 }
 
@@ -57,8 +67,11 @@ where
     V: Clone,
 {
     fn new(num_buckets: usize) -> Self {
-        let buckets = vec![LinkedList::new(); num_buckets];
-        CustomListMap { buckets, num_buckets }
+        let mut buckets = Vec::with_capacity(num_buckets);
+        for _ in 0..num_buckets {
+            buckets.push(LinkedList::new());
+        }
+        return CustomListMap { buckets, num_buckets };
     }
 
     fn hash(&self, key: &K) -> usize {
@@ -91,10 +104,15 @@ where
     fn remove(&mut self, key: &K) {
         let index = self.hash(key);
         let bucket = &mut self.buckets[index];
-        let position = bucket.iter().position(|entry| &entry.key == key);
 
-        if let Some(pos) = position {
-            bucket.remove(pos);
+        let mut current = 0;
+        while current < bucket.len() {
+            if bucket[current].key == *key {
+                bucket.remove(current);
+                return;
+            } else {
+                current += 1;
+            }
         }
     }
 
@@ -111,6 +129,48 @@ where
     K: Hash + Eq,
     V: Clone,
 {
-    //stuff
-}
+    fn new(num_buckets: usize) -> Self {
+        let mut buckets = Vec::with_capacity(num_buckets);
+        for _ in 0..num_buckets {
+            buckets.push(Vec::new());
+        }
+        return CustomVecMap { buckets, num_buckets };
+    }
 
+    fn hash(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        return (hasher.finish() % self.num_buckets as u64) as usize;
+    }
+
+    fn insert(&mut self, key: K, value: V) {
+        let index = self.hash(&key);
+        let bucket = &mut self.buckets[index];
+
+        for entry in bucket.iter_mut() {
+            if entry.key == key {
+                entry.value = value.clone();
+                return;
+            }
+        }
+        bucket.push(Entry { key, value });
+    }
+
+    fn get(&self, key: &K) -> Option<V> {
+        let index = self.hash(key);
+        self.buckets[index]
+            .iter()
+            .find(|entry| &entry.key == key)
+            .map(|entry| entry.value.clone())
+    }
+
+    fn remove(&mut self, key: &K) {
+        let index = self.hash(key);
+        let bucket = &mut self.buckets[index];
+        let position = bucket.iter().position(|entry| &entry.key == key);
+
+        if let Some(pos) = position {
+            bucket.remove(pos);
+        }
+    }
+}
